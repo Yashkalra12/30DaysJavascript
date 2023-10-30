e373d3909a1540f2c02d663b230297e02b36bf2b
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2023-10-30 23:21:14 +1100

    Reatupload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    // <nav className="main_container ">
+    //   <div className="in_container">
+    //     <span className="in_container">Todo App : FEE Project</span>
+    //   </div>
+    // </nav>
+
+    <nav>
+      <span className="im_container">Todo App</span>
+    </nav>
+  );
+}
+
+export default Navbar;

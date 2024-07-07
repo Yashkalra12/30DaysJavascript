ca69df87e486213f19c2d3049f1ba7d6828831db
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-07-07 12:37:00 +0530

    Day 1

---
+++ Day-1/datatypes.js
@@ -0,0 +1,36 @@
+
+
+// Use typeof operator to check data types
+console.log(typeof stringVariable); // string
+console.log(typeof booleanVariable); // boolean
+console.log(typeof undefinedVariable); // undefined
+console.log(typeof nullVariable); // object (special case in JavaScript)
+
+// Declare four variables without assigning values
+let var1;
+let var2;
+let var3;
+let var4;
+
+// Declare four variables with assigned values
+let assignedVar1 = "Assigned";
+let assignedVar2 = 100;
+let assignedVar3 = false;
+let assignedVar4 = null;
+
+// Declare variables to store personal details in multiple lines
+let firstName = "John";
+let lastName = "Doe";
+let maritalStatus = "Single";
+let country = "USA";
+let age = 25;
+
+// Declare variables to store personal details in a single line
+let firstNameSingle = "John", lastNameSingle = "Doe", maritalStatusSingle = "Single", countrySingle = "USA", ageSingle = 25;
+
+// Declare two variables myAge and yourAge and log to the browser console
+let myAge = 25;
+let yourAge = 30;
+
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
---
+++ Day-1/variable.js
@@ -0,0 +1,4 @@
+let stringvar="Yash";
+let booleanvar=true;
+let undefinedvar;
+let nullvariable=null;
